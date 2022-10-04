3ffc01e9b90099900abacb9a09ab1d3c81865807
ben <brunette.ben@gmail.com>
2022-10-04 18:29:59 +0200

    read file from input

--- AudioChannelSplitter/AudioChannelSplitter.cpp
+++ AudioChannelSplitter/AudioChannelSplitter.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdint>
+#include <string>
+#include "wav_header.h"
+ 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    static const uint16_t BUFFER_SIZE = 4096;
+    wav_hdr wavHeader;
+    int headerSize = sizeof(wav_hdr), fileLength = 0;
+
+    const char* filePath;
+    string input;
+    if (argc <= 1)
+    {
+        cout << "Enter file name: ";
+        cin >> input;
+        cin.get();
+        filePath = input.c_str();
+    }
+    else
+    {
+        filePath = argv[1];
+        cout << "File name from args: " << filePath << endl;
+    }
 }
